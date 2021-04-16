68be69f0b3832e2746588c5bd25157383f7b8db3
testsite1212 <82283132+testsite1212@users.noreply.github.com>
2021-04-16 19:56:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnopen = document.querySelector('button');
+let input = document.querySelector('input');
+
+btnopen.addEventListener('click', () => {
+	window.open('https://testsite1212.github.io/CourseMacth/htt.html');
+})
